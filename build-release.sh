53b441fd9a71871c580ac24ae1295685864e46b3
Owen Nelson <onelson@gmail.com>
2016-11-30 19:30:13 -0800

    come on now

--- build-release.sh
+++ build-release.sh
@@ -4,10 +4,10 @@ set -e
 
 OS=$(uname -s)
 ARCH=$(uname -p)
-RELEASE=omn-labs-${OS,,}-${ARCH,,}
+RELEASE="omn-labs-${OS,,}-${ARCH,,}"
 cargo test
 cargo build --release
 mkdir -p dist/$RELEASE
 cp target/release/omn-labs dist/$RELEASE/
-cp -r assets dist/$RELEASE/
+cp -r assets dist/${RELEASE}/
 cd dist && zip ${RELEASE}.zip -r ${RELEASE}
