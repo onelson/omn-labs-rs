42c6ac568ce2768e44214e7f39661c000c4ace90
Owen Nelson <onelson@gmail.com>
2017-07-23 23:04:15 -0700

    tiny readline loop

---
+++ examples/simple-prompt-pda.rs
@@ -0,0 +1,29 @@
+extern crate rustyline;
+extern crate omn_labs;
+
+use omn_labs::state::{State, StateMachine};
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
+
+fn main() {
+    let mut rl = Editor::<()>::new();
+    loop {
+        println!("Gimmie a val!");
+        let readline = rl.readline(">> ");
+
+        match readline {
+            Err(_) => {
+                println!("Bye!");
+                break;
+            }
+            Ok(val) => {
+                if ["exit", "quit"].contains(&val.as_str()) {
+                    println!("Bye!");
+                    break;
+                }
+                println!("val: {}", val)
+            }
+        }
+    }
+}
